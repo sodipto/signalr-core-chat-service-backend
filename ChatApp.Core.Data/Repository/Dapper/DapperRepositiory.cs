44cfc651b78e9ecd554697eeb7a30c8632750828
Sodipto Saha <badhansaha444@gmail.com>
2023-03-14 19:47:49 +0600

    dapper repository

---
+++ ChatApp.Core.Data/Repository/Dapper/DapperRepositiory.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using System.Data;
+using static Dapper.SqlMapper;
+
+namespace ChatApp.Core.Data
+{
+    public class DapperRepositiory : IDapperRepositiory
+    {
+        #region Fields
+        private readonly ApplicationDbContext _dbContext;
+        #endregion
+
+        #region Ctor
+        public DapperRepositiory(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        #endregion
+
+        #region Methods
+
+        public async Task<IReadOnlyList<T>> QueryAsync<T>(string sql, object param = null, IDbTransaction transaction = null)
+        {
+            return (await _dbContext.Connection.QueryAsync<T>(sql, param, transaction)).AsList();
+        }
+
+        public async Task<T> QueryFirstOrDefaultAsync<T>(string sql, object param = null, IDbTransaction transaction = null)
+        {
+            return await _dbContext.Connection.QueryFirstOrDefaultAsync<T>(sql, param, transaction);
+        }
+        public async Task<T> QuerySingleAsync<T>(string sql, object param = null, IDbTransaction transaction = null)
+        {
+            return await _dbContext.Connection.QuerySingleAsync<T>(sql, param, transaction);
+        }
+
+        public async Task<GridReader> QueryMultipleAsync(string sql, object param = null, IDbTransaction transaction = null)
+        {
+            return await _dbContext.Connection.QueryMultipleAsync(sql, param, transaction);
+        }
+
+        public async Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null)
+        {
+            return await _dbContext.Connection.ExecuteAsync(sql, param, transaction);
+        }
+        #endregion
+    }
+}
---
+++ ChatApp.Core.Data/Repository/Dapper/IDapperRepositiory.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using static Dapper.SqlMapper;
+
+namespace ChatApp.Core.Data
+{
+    public interface IDapperRepositiory
+    {
+        Task<IReadOnlyList<T>> QueryAsync<T>(string sql, object param = null, IDbTransaction transaction = null);
+        Task<T> QueryFirstOrDefaultAsync<T>(string sql, object param = null, IDbTransaction transaction = null);
+        Task<T> QuerySingleAsync<T>(string sql, object param = null, IDbTransaction transaction = null);
+        Task<GridReader> QueryMultipleAsync(string sql, object param = null, IDbTransaction transaction = null);
+        Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null);
+    }
+}
