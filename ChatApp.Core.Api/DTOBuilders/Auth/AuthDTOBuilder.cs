ff104950745a566e90049a9aae788a9a1e9fc4df
Sodipto Saha <badhansaha444@gmail.com>
2023-03-25 20:42:23 +0600

    login method

--- ChatApp.Core.Api/DTOBuilders/Auth/AuthDTOBuilder.cs
+++ ChatApp.Core.Api/DTOBuilders/Auth/AuthDTOBuilder.cs
@@ -1,6 +1,15 @@
-﻿namespace ChatApp.Core.Api.DTOBuilders
+﻿using ChatApp.Core.Utils.DTOs;
+using System.Threading.Tasks;
+
+namespace ChatApp.Core.Api.DTOBuilders
 {
     public static class AuthDTOBuilder
     {
+        public static async Task<AuthDTO> Login(string email, string password)
+        {
+            var dto=new AuthDTO();
+
+            return dto;
+        }
     }
 }
